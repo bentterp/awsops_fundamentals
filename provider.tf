9891309822db613bcd52a5cbf1676558a11761da
Bent Terp <bent.terp@basefarm.com>
2018-03-28 10:27:38 +0200

    bent my name

--- provider.tf
+++ provider.tf
@@ -1,15 +1,16 @@
 terraform {
-  required_version = "~> 0.11.1"
+  required_version = "~> 0.11"
 	backend "s3" {
 		role_arn     = "arn:aws:iam::552687213402:role/admin"
 		external_id  = "BF-AWSOpsLab"
 		bucket         = "bf-awslab-tfstate"
-		key            = "fundamentals/####.tfstate"
+		key            = "fundamentals/bent.tfstate"
 		dynamodb_table = "TerraformLocks"
 		region         = "eu-west-1"
 	}
 }
 provider "aws" {
+  version = "~> 1.12"
 	region  = "${var.region}"
 	assume_role {
 		role_arn     = "arn:aws:iam::552687213402:role/admin"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
 region = "eu-central-1"
-costcenter = "%%%%"
-environment = "####"
+costcenter = "PS"
+environment = "bent"
